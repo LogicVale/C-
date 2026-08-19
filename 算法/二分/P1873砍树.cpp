5ce2cb22d447da0d6f6c1336824d287f617b2fc5
本地纸巾盒 <16428776+local-tissue-box@user.noreply.gitee.com>
2026-08-19 15:36:06 +0800

    P1873砍树

---
+++ 算法/二分/P1873砍树.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int a[1000010];
+
+bool check(int h) {
+    long long sum = 0;
+    for (int i=0;i<n;i++) {
+        if (a[i]>h) {
+            sum += a[i]-h;
+        }
+    }
+    if (sum >= m) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n >> m;
+    for (int i=0;i<n;i++) cin >> a[i];
+    sort(a,a+n);
+    int l = 0;
+    int r = a[n-1];
+    int res = 0;
+    while (l<=r) {
+        int mid = (l+r)/2;
+        if (check(mid)) {
+            res = mid;
+            l = mid + 1;
+        } else {
+            r = mid - 1;
+        }
+    }
+    cout << res;
+}
+
